9168528a08fbca251ca6cd15084dd5ade6420fc5
devendor231 <juli.steen@t-online.de>
2025-05-18 15:05:29 +0200

    hoi

---
+++ src/CommunicationChannel.java
@@ -0,0 +1,3 @@
+enum CommunicationChannel {
+    EMAIL, SMS, PUSH
+}
---
+++ src/Notification.java
@@ -0,0 +1,11 @@
+class Notification {
+    private String message;
+
+    public Notification(String message) {
+        this.message = message;
+    }
+
+    public void deliver(CommunicationChannel channel) {
+        System.out.println("[MOCK " + channel + "] " + message);
+    }
+}
